67e00946d3f115e37134a9d17d40598552e0e006
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2024-11-25 14:05:15 +0800

    feat: Add guidance note to enricher configuration page (#21)

--- src/ExternalSearch.Providers.PermId/Constants.cs
+++ src/ExternalSearch.Providers.PermId/Constants.cs
@@ -11,7 +11,39 @@ namespace CluedIn.ExternalSearch.Providers.PermId
         public const string ComponentName = "PermId";
         public const string ProviderName = "PermId";
         public static readonly Guid ProviderId = Core.Constants.ExternalSearchProviders.PermId;
-
+        public const string Instruction = """
+            [
+              {
+                "type": "bulleted-list",
+                "children": [
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the entity type to specify the golden records you want to enrich. Only golden records belonging to that entity type will be enriched."
+                      }
+                    ]
+                  },
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the vocabulary keys to provide the input for the enricher to search for additional information. For example, if you provide the website vocabulary key for the Web enricher, it will use specific websites to look for information about companies. In some cases, vocabulary keys are not required. If you don't add them, the enricher will use default vocabulary keys."
+                      }
+                    ]
+                  },
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the API key to enable the enricher to retrieve information from a specific API. For example, the Vatlayer enricher requires an access key to authenticate with the Vatlayer API."
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+            """;
         public struct KeyName
         {
             public const string ApiToken = "apiToken";
@@ -66,7 +98,10 @@ namespace CluedIn.ExternalSearch.Providers.PermId
             //}
         };
 
-        public static Guide Guide { get; set; } = null;
+        public static Guide Guide { get; set; } = new Guide
+        {
+            Instructions = Instruction
+        };
         public static IntegrationType IntegrationType { get; set; } = IntegrationType.Enrichment;
     }
 }
